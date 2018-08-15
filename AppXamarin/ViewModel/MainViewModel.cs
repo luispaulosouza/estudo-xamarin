0578cd99f95c8da4b5a531bfe22a3b73113e2002
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-08-15 10:06:16 -0300

    :tada: Enviado APP

---
+++ AppXamarin/ViewModel/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Xamarin.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace AppXamarin.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+
+        private string _login;
+        public string Login
+        {
+            get
+            {
+                return _login;
+            }
+            set
+            {
+                SetProperty(ref _login, value);
+            }
+        }
+
+        private string _senha;
+        public string Senha
+        {
+            get
+            {
+                return _senha;
+            }
+            set
+            {
+                SetProperty(ref _senha, value);
+            }
+        }
+
+
+        public Command Logar { get; set; }
+        public MainViewModel()
+        {
+             Logar = new Command(HandleAction);
+        }
+
+
+
+        void HandleAction()
+        {
+            string teste = Login;
+            Login = "Miguel";
+        }
+
+    }
+}
